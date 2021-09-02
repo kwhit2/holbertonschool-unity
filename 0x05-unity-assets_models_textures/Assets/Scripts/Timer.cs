f2832ff24ac6947fee2d7c1b8e5b367c87bdca58
Kyle Whitten <kfw2@outlook.com>
2021-09-02 15:28:29 -0500

    Update

--- 0x05-unity-assets_models_textures/Assets/Scripts/Timer.cs
+++ 0x05-unity-assets_models_textures/Assets/Scripts/Timer.cs
@@ -3,21 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using System.Diagnostics;
+using System;
 
 public class Timer : MonoBehaviour
 {
     public Text TimerText;
-    private Stopwatch timer = new Stopwatch();
+
+    float elapsedTime;
+
+    private TimeSpan activeTime;
+
+    // goes with commented out Stopwatch code in the Update method
+    // private Stopwatch timer = new Stopwatch();
 
     // Start is called before the first frame update
     void Start()
     {
-        timer.Start();
+        // was to start Stopwatch but could not get milliseconds to 2 decimal places
+        // timer.Start();
     }
 
     // Update is called once per frame
     void Update()
     {
-        TimerText.text = timer.Elapsed.Minutes + ":" + timer.Elapsed.Seconds.ToString("00") + "." + timer.Elapsed.Milliseconds.ToString("00");
+        elapsedTime += Time.deltaTime;
+        activeTime = TimeSpan.FromSeconds(elapsedTime);
+        string activeTimeStr = activeTime.ToString("mm':'ss'.'ff");
+        TimerText.text = activeTimeStr;
+        // could not get timer to display 2 decimal places for milliseconds this way
+        // TimerText.text = timer.Elapsed.Minutes + ":" + timer.Elapsed.Seconds.ToString("00") + "." + timer.Elapsed.Milliseconds.ToString("00");
     }
 }
--- 0x05-unity-assets_models_textures/README.md
+++ 0x05-unity-assets_models_textures/README.md
@@ -16,3 +16,7 @@
 - How to create UI elements with image components
 - What is a Rigidbody and what is a Character Controller and what are the pros and cons of using each
 - What is a Quaternion
+
+## Credits
+- [Skyboxes: Farland Skies - Cloudy Crown](https://assetstore.unity.com/packages/2d/textures-materials/sky/farland-skies-cloudy-crown-60004)
+- [Models: Kenney's Nature Pack Extended](https://kenney.nl/assets/nature-pack-extended)
