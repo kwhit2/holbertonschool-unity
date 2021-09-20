1876327de7c62e55dae230320019cf7cabafd340
Kyle Whitten <kfw2@outlook.com>
2021-09-20 13:01:13 -0500

    Trying to invert

--- 0x06-unity-assets_ui/Assets/Scripts/CameraController.cs
+++ 0x06-unity-assets_ui/Assets/Scripts/CameraController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    public bool isInverted;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,11 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // Invert mouse controls
+        if (isInverted)
+        {
+            var MouseY = Input.GetAxis("Mouse Y") * Time.deltaTime * 40.0f;
+            transform.Rotate(-MouseY, 0, 0);
+        }
     }
 }
