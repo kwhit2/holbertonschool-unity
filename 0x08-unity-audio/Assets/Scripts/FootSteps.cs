de396d011d9e83bb7dbcc1fc1877da69921812ab
Kyle Whitten <kfw2@outlook.com>
2021-10-20 16:39:00 -0500

    Landing after respawn sound

--- 0x08-unity-audio/Assets/Scripts/FootSteps.cs
+++ 0x08-unity-audio/Assets/Scripts/FootSteps.cs
@@ -6,18 +6,22 @@ public class FootSteps : MonoBehaviour
 {
     CharacterController cc;
 
+    public AudioSource footsteps_running_grass;
+
     // Start is called before the first frame update
     void Start()
     {
         cc = GetComponent<CharacterController>();
+        footsteps_running_grass = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (cc.isGrounded == true && cc.velocity.magnitude > 2f && GetComponent<AudioSource>().isPlaying == false)
+        if (cc.isGrounded == true && cc.velocity.magnitude > 2f && footsteps_running_grass.isPlaying == false)
         {
-            GetComponent<AudioSource>().Play();
+            // footsteps
+            footsteps_running_grass.Play();
         }
     }
 }
