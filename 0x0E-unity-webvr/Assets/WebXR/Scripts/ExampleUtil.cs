2a057746e34ea66101add624642e1ceca7cbbe01
Kyle Whitten <kfw2@outlook.com>
2022-03-02 16:13:22 -0600

    Upload

---
+++ 0x0E-unity-webvr/Assets/WebXR/Scripts/ExampleUtil.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+internal static class ExampleUtil
+{
+    public static bool isPresent()
+    {
+        var xrDisplaySubsystems = new List<XRDisplaySubsystem>();
+        SubsystemManager.GetInstances<XRDisplaySubsystem>(xrDisplaySubsystems);
+        foreach (var xrDisplay in xrDisplaySubsystems)
+        {
+            if (xrDisplay.running)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+public class CheckXRDisplay : MonoBehaviour
+{
+    void Awake()
+    {
+        Debug.Log("Do we have an Active Display? " + ExampleUtil.isPresent().ToString());
+    }
+}
---
+++ 0x0E-unity-webvr/README.md
@@ -0,0 +1,10 @@
+# 0x0E. Unity - WebVR
+- [Project Page](https://intranet.hbtn.io/projects/870)
+
+## Learning Objectives
+- What is WebGL
+- What is polyfill
+- How to deploy for WebGL from Unity
+- What is WebXR and what is its purpose
+- How to design and accommodate for both VR input and keyboard/mouse input
+- What is OpenUPM
